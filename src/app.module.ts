bce32e3e095e2f268339d504d8a3cced4a1da906
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-09-16 19:27:01 +0400

    add file c

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule],
   controllers: [AppController],
   providers: [AppService],
+  imports: [UsersModule],
 })
 export class AppModule {}
