9d5f1829fec5aa0b317f4221e38d36301c1468bc
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-09-19 01:01:56 +0400

    edit files name

--- src/Categories/categories.module.ts
+++ src/Categories/categories.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
-import { Category } from './category.entity';
-import { Product } from '../products/product.entity';
+import { CategoriesService } from './categories.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category, Product])],
-  providers: [CategoriesService],
   controllers: [CategoriesController],
-  exports: [CategoriesService],
+  providers: [CategoriesService],
 })
 export class CategoriesModule {}
